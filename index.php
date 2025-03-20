811e2971c23ad0776f2c3d4afd2511cc9c83ab63
Dorian FOURNIER LERIN <fourni14do@lycee-ndduroc.com>
2025-03-20 21:11:18 +0100

    Adding lock for secure

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Index</title>
+    <title>Valet Page Manager</title>
     <!-- Bootstrap core CSS -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
@@ -56,31 +56,45 @@
         return !empty($crt);
     }
 
-    function card($name, $link) {
-        return '<div class="col-2">
-                    <div class="card" style="width: 18rem;">
+    function lock($secure = false) {
+        if ($secure) {
+            $html = '<a href="" class="btn btn-success">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 448 512"><path d="M144 144l0 48 160 0 0-48c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192l0-48C80 64.5 144.5 0 224 0s144 64.5 144 144l0 48 16 0c35.3 0 64 28.7 64 64l0 192c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 256c0-35.3 28.7-64 64-64l16 0z"/></svg>
+                    </a>';
+        }else{
+            $html = '<a href="" class="btn btn-danger">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 576 512"><path d="M352 144c0-44.2 35.8-80 80-80s80 35.8 80 80l0 48c0 17.7 14.3 32 32 32s32-14.3 32-32l0-48C576 64.5 511.5 0 432 0S288 64.5 288 144l0 48L64 192c-35.3 0-64 28.7-64 64L0 448c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-192c0-35.3-28.7-64-64-64l-32 0 0-48z"/></svg>
+                    </a>';
+        }
+        return $html;
+    }
+
+    function card($name, $link, $secure = false) {
+
+        return '<div class="col-3">
+                    <div class="card">
                         <div class="card-body">
 						    <h5 class="card-title">'.$name.'</h5>
-						    <a href="'.$link.'" class="btn btn-primary">Accéder</a>
+						    <a target="_blank" href="'.$link.'" class="btn btn-primary">Accéder</a>
+						    '.lock($secure).'
 				        </div>
 				    </div>
 				</div>';
     }
 
-
     foreach(glob('/home/'.$user.'/.config/valet/Sites/*',GLOB_ONLYDIR) as $dir){
         $dir = basename($dir);
-        $link = 'https://'.$dir.'test';
 
-        if ($dir != 'accueil') {
-            echo card($dir, $link);
-//            echo '<div class="col-2"><div class="card" style="width: 18rem;">';
-//            echo '<div class="card-body">
-//						<h5 class="card-title">'.$dir.'</h5>
-//						<a href="https://" class="btn btn-primary">Accéder</a>
-//				    </div>
-//				</div>
-//				</div>';
+        if (isSecure($dir)) {
+            $link = 'https://'.$dir.'.test';
+            $secure = true;
+        }else{
+            $link = 'http://'.$dir.'.test';
+            $secure = false;
+        }
+
+        if ($dir.'.test' != $_SERVER['HTTP_HOST']) {
+            echo card($dir, $link, $secure);
         }
 
 	}
@@ -95,4 +109,4 @@
       
   </body>
 </html>
-
+<?php
