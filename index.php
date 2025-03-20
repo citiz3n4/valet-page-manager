20a5ebda283e149e60c25c2ddb9466b10bbee791
Dorian FOURNIER LERIN <fourni14do@lycee-ndduroc.com>
2025-03-20 22:01:23 +0100

    Adding class

--- index.php
+++ index.php
@@ -50,51 +50,12 @@
 	<?php
     $user = exec('whoami');
 
-    function isSecure($fileName) {
-        global $user;
-        $crt = glob("/home/$user/.config/valet/Certificates/$fileName.test.crt");
-        return !empty($crt);
-    }
-
-    function lock($secure = false) {
-        if ($secure) {
-            $html = '<a href="" class="btn btn-success">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 448 512"><path d="M144 144l0 48 160 0 0-48c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192l0-48C80 64.5 144.5 0 224 0s144 64.5 144 144l0 48 16 0c35.3 0 64 28.7 64 64l0 192c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 256c0-35.3 28.7-64 64-64l16 0z"/></svg>
-                    </a>';
-        }else{
-            $html = '<a href="" class="btn btn-danger">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 576 512"><path d="M352 144c0-44.2 35.8-80 80-80s80 35.8 80 80l0 48c0 17.7 14.3 32 32 32s32-14.3 32-32l0-48C576 64.5 511.5 0 432 0S288 64.5 288 144l0 48L64 192c-35.3 0-64 28.7-64 64L0 448c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-192c0-35.3-28.7-64-64-64l-32 0 0-48z"/></svg>
-                    </a>';
-        }
-        return $html;
-    }
-
-    function card($name, $link, $secure = false) {
-
-        return '<div class="col-3">
-                    <div class="card">
-                        <div class="card-body">
-						    <h5 class="card-title">'.$name.'</h5>
-						    <a target="_blank" href="'.$link.'" class="btn btn-primary">Accéder</a>
-						    '.lock($secure).'
-				        </div>
-				    </div>
-				</div>';
-    }
-
     foreach(glob('/home/'.$user.'/.config/valet/Sites/*',GLOB_ONLYDIR) as $dir){
         $dir = basename($dir);
-
-        if (isSecure($dir)) {
-            $link = 'https://'.$dir.'.test';
-            $secure = true;
-        }else{
-            $link = 'http://'.$dir.'.test';
-            $secure = false;
-        }
+        $link = new Link($dir);
 
         if ($dir.'.test' != $_SERVER['HTTP_HOST']) {
-            echo card($dir, $link, $secure);
+            echo $link->card();
         }
 
 	}
@@ -110,3 +71,45 @@
   </body>
 </html>
 <?php
+class Link {
+    public bool $secure = false;
+    public string $fileName;
+    public string $link;
+
+    public function __construct($fileName)
+    {
+        $this->secure = $this->isSecure($fileName);
+        $this->fileName = $fileName;
+        $this->link = $this->secure ? 'https://'.$this->fileName.'.test' : 'http://'.$this->fileName.'.test';
+    }
+
+    public function lock() {
+        if ($this->secure) {
+            $html = '<button class="btn btn-success">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 448 512"><path d="M144 144l0 48 160 0 0-48c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192l0-48C80 64.5 144.5 0 224 0s144 64.5 144 144l0 48 16 0c35.3 0 64 28.7 64 64l0 192c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 256c0-35.3 28.7-64 64-64l16 0z"/></svg>
+                    </button>';
+        }else{
+            $html = '<button class="btn btn-danger">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 576 512"><path d="M352 144c0-44.2 35.8-80 80-80s80 35.8 80 80l0 48c0 17.7 14.3 32 32 32s32-14.3 32-32l0-48C576 64.5 511.5 0 432 0S288 64.5 288 144l0 48L64 192c-35.3 0-64 28.7-64 64L0 448c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-192c0-35.3-28.7-64-64-64l-32 0 0-48z"/></svg>
+                    </button>';
+        }
+        return $html;
+    }
+    public function isSecure($fileName) {
+        global $user;
+        $crt = glob("/home/$user/.config/valet/Certificates/$fileName.test.crt");
+        return !empty($crt);
+    }
+    public function card() {
+
+        return '<div class="col-3">
+                    <div class="card">
+                        <div class="card-body">
+						    <h5 class="card-title">'.$this->fileName.'</h5>
+						    <a target="_blank" href="'.$this->link.'" class="btn btn-primary">Accéder</a>
+						    '.$this->lock().'
+				        </div>
+				    </div>
+				</div>';
+    }
+}
