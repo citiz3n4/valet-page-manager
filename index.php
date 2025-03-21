13621defb99a496bfa2964802d1a0a3e7c46c235
Dorian FOURNIER LERIN <fourni14do@lycee-ndduroc.com>
2025-03-21 18:20:21 +0100

    Correcting PhpStorm error

--- index.php
+++ index.php
@@ -13,26 +13,6 @@ $domain = '.'.$config->domain;
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-    </style>
-
-    
-    <!-- Custom styles for this template -->
-    <link href="navbar-top.css" rel="stylesheet">
   </head>
   <body>
     
@@ -93,9 +73,6 @@ $domain = '.'.$config->domain;
 	</div>
 </main>
 
-
-    <script src="/docs/5.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
       
   </body>
 </html>
@@ -112,7 +89,7 @@ class Link {
         $this->link = $this->secure ? 'https://'.$this->fileName.'.test' : 'http://'.$this->fileName.'.test';
     }
 
-    public function lock() {
+    public function lock() : string {
         if ($this->secure) {
             $html = '<div class="btn btn-success pe-none">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 448 512"><path d="M144 144l0 48 160 0 0-48c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192l0-48C80 64.5 144.5 0 224 0s144 64.5 144 144l0 48 16 0c35.3 0 64 28.7 64 64l0 192c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 256c0-35.3 28.7-64 64-64l16 0z"/></svg>
@@ -124,12 +101,12 @@ class Link {
         }
         return $html;
     }
-    public function isSecure($fileName) {
+    public function isSecure($fileName) : bool {
         global $user;
         $crt = glob("/home/$user/.config/valet/Certificates/$fileName.test.crt");
         return !empty($crt);
     }
-    public function card() {
+    public function card() : string {
 
         return '<div class="col-3">
                     <div class="card mb-4">
