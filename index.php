277d2c1be717d91fa9fff973a4abf398693887fa
Dorian FOURNIER LERIN <fourni14do@lycee-ndduroc.com>
2025-03-21 17:48:19 +0100

    Adding park links

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
       <ul class="navbar-nav me-auto mb-2 mb-md-0">
         <li class="nav-item">
           <a class="nav-link" href="http://phpmyadmin.test">
-			  <img src="../phpmyadmin/favicon.ico" width="16">
+			  <img src="http://phpmyadmin.test/favicon.ico" width="16">
           </a>
         </li>
       </ul>
@@ -49,18 +49,45 @@
 	
 	<?php
     $user = exec('whoami');
-
-    foreach(glob('/home/'.$user.'/.config/valet/Sites/*',GLOB_ONLYDIR) as $dir){
-        $dir = basename($dir);
-        $link = new Link($dir);
-
-        if ($dir.'.test' != $_SERVER['HTTP_HOST']) {
-            echo $link->card();
-        }
-
-	}
-
-	?>
+    $config = file_get_contents("/home/$user/.config/valet/config.json");
+    $config = json_decode($config);
+    $domain = '.'.$config->domain;
+    ?>
+        <?php foreach ($config->paths as $path): ?>
+            <?php if (basename($path) == 'Sites'): ?>
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <h5 class="card-title">Links</h5>
+                        <div class="row">
+                            <?php
+                            foreach(glob($path.'/*',GLOB_ONLYDIR) as $dir){
+                                if ($dir.$domain != $_SERVER['HTTP_HOST']) {
+                                    $dir = basename($dir);
+                                    $link = new Link($dir);
+                                    echo $link->card();
+                                }
+                            }?>
+                        </div>
+                    </div>
+                </div>
+            <?php else: ?>
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <h5 class="card-title">Park links</h5>
+                        <div class="row">
+                            <?php
+                            foreach(glob($path.'/*',GLOB_ONLYDIR) as $dir){
+                                if ($dir.$domain != $_SERVER['HTTP_HOST']) {
+                                    $dir = basename($dir);
+                                    $link = new Link($dir);
+                                    echo $link->card();
+                                }
+                            }?>
+                        </div>
+                    </div>
+                </div>
+            <?php endif; ?>
+        <?php endforeach; ?>
 	</div>
 </main>
 
@@ -85,13 +112,13 @@ class Link {
 
     public function lock() {
         if ($this->secure) {
-            $html = '<button class="btn btn-success">
+            $html = '<div class="btn btn-success pe-none">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 448 512"><path d="M144 144l0 48 160 0 0-48c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192l0-48C80 64.5 144.5 0 224 0s144 64.5 144 144l0 48 16 0c35.3 0 64 28.7 64 64l0 192c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 256c0-35.3 28.7-64 64-64l16 0z"/></svg>
-                    </button>';
+                    </div>';
         }else{
-            $html = '<button class="btn btn-danger">
+            $html = '<div class="btn btn-danger pe-none">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 576 512"><path d="M352 144c0-44.2 35.8-80 80-80s80 35.8 80 80l0 48c0 17.7 14.3 32 32 32s32-14.3 32-32l0-48C576 64.5 511.5 0 432 0S288 64.5 288 144l0 48L64 192c-35.3 0-64 28.7-64 64L0 448c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-192c0-35.3-28.7-64-64-64l-32 0 0-48z"/></svg>
-                    </button>';
+                    </div>';
         }
         return $html;
     }
@@ -103,9 +130,9 @@ class Link {
     public function card() {
 
         return '<div class="col-3">
-                    <div class="card">
+                    <div class="card mb-4">
                         <div class="card-body">
-						    <h5 class="card-title">'.$this->fileName.'</h5>
+						    <h5 class="card-title">'.$this->fileName.'</h5>   
 						    <a target="_blank" href="'.$this->link.'" class="btn btn-primary">Accéder</a>
 						    '.$this->lock().'
 				        </div>
