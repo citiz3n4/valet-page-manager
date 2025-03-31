7050f3d74b5837bcd5258d2bfcb3be84abc6aee8
Erwan Gallenne <erwan@gallenne.fr>
2025-03-31 08:38:33 +0200

    update config.json creation and getting data

--- index.php
+++ index.php
@@ -19,17 +19,17 @@ if (isset($_POST['unlink'])) {
     <title>Valet Page Manager</title>
     <!-- Bootstrap core CSS -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+      <style>
+          .vl {
+              border-left: 2px solid;
+              height: 30px;
+              color: #6F7378;
+              margin-top: 5px;
+      }
+      </style>
   </head>
   <body>
-  <style>
-      .vl {
-          border-left: 2px solid;
-          height: 30px;
-          color: #6F7378;
-          margin-top: 5px;
-      }
-  </style>
 
     
 <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
