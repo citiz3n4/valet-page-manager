1ebce2a46cbb7e77c1fc88448075e7ef7d9f3045
Titouan <titouan@asnard.fr>
2025-04-02 17:52:09 +0200

    Working on a create button to create a whole project etc

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $configValet = file_get_contents("/home/$user/.config/valet/config.json");
 $configValet = json_decode($configValet);
 $domain = '.'.$configValet->domain;
 Config::setTld($domain);
-$_SESSION['config'] = isset($_SESSION['config']) ? $_SESSION['config'] : false;
+$_SESSION['config'] = $_SESSION['config'] ?? false;
 
 
 function valetUnlink($project): void {
@@ -83,7 +83,7 @@ class Link {
     {
 
         foreach (Config::get('exclude') as $excluded) {
-            if ($excluded == $this->fileName) {
+            if (strtolower($excluded) == strtolower($this->fileName)) {
                 return true;
             }
         }
@@ -211,6 +211,7 @@ if (isset($_POST['include'])) {
     <link rel="icon" href="<?= Config::get('app')['icon'] ?>">
     <!-- Bootstrap core CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <style>
         .vl {
@@ -239,7 +240,10 @@ if (isset($_POST['include'])) {
                 )
                 ?>
             </ul>
-            <div class="float-end">
+            <div class="float-end mx-1">
+                <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#createModal">Create</button>
+            </div>
+            <div class="float-end mx-1">
                 <a href=".?config=1" class="btn <?= ($_SESSION['config']??false) ? 'btn-primary' : 'btn-secondary' ?>">Settings</a>
             </div>
         </div>
@@ -289,7 +293,68 @@ if (isset($_POST['include'])) {
         <?php endforeach; ?>
     </div>
 </main>
-
-
+<div class="modal fade" id="createModal" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="createModal">Create a new project</h5>
+                <button type="button" class="close btn" data-bs-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form action="#" method="POST">
+                <div class="modal-body">
+                    <label for="type">What type of project do you want to create ? </label>
+                    <div id="type" class="my-2">
+                        <div class="my-1">
+                            <input type="radio" id="api" name="type" value="api"/>
+                            <label for="name">API</label>
+                        </div>
+                        <div class="my-1">
+                            <input type="radio" id="monolithic" name="type" value="monolithic"/>
+                            <label for="name">Monolithic</label>
+                        </div>
+                        <div class="my-1">
+                            <input type="radio" id="headless" name="type" value="headless"/>
+                            <label for="name">Headless</label>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="my-2">
+                        <label for="name">Project's name : </label>
+                        <input class="form-control" id="name" name="name" oninput="document.getElementById('submitButton').innerHTML = 'Create '+this.value"/>
+                    </div>
+                    <hr>
+                    <label for="dbType">What kind of SGBD do you want to use ?</label>
+                    <div id="dbType" class="my-2">
+                        <div class="my-1">
+                            <input type="radio" id="sqlite" name="dbType" value="sqlite" oninput="document.getElementById('dbName').disable = true"/>
+                            <label for="name">SQLite</label>
+                        </div>
+                        <div class="my-1">
+                            <input type="radio" id="mysql" name="dbType" value="mysql"/>
+                            <label for="name">MySQL</label>
+                        </div>
+                    </div>
+                    <div class="my-2">
+                        <label for="dbName">Database's name : </label>
+                        <input class="form-control" id="dbName" name="dbName"/>
+                    </div>
+                    <div class="my-2">
+                        <label for="dbUser">Database's username : </label>
+                        <input class="form-control" id="dbUser" name="dbUser"/>
+                    </div>
+                    <div class="my-2">
+                        <label for="dbPassword">Database's user's password : </label>
+                        <input type="password" class="form-control" id="dbPassword" name="dbPassword"/>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary" id="submitButton">Create the project</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 </body>
 </html>
